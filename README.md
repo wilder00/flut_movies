7340a354eac1f99c6720315879a1fdab02ab64c3
Wilder TM <nadie437@gmail.com>
2021-02-20 20:49:24 -0500

    agregando packetes:  Swapper y poniendo algunos estilos bases

--- README.md
+++ README.md
@@ -14,3 +14,27 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+## paketes
+
+### flutter_Swiper
+
+El paquete de swiper lo podemos obtener del sugiente [enlace](https://pub.dev/packages/flutter_swiper)
+
+#### registrar dependencia
+Agregar lo siguiente en pubspec.yaml:
+```
+dependencies:
+  flutter_swiper: ^1.1.6
+```
+
+para instalarlo:
+```
+flutter pub get
+```
+
+Para importarlo
+```
+import 'package:flutter_swiper/flutter_swiper.dart';
+```
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text("Peliculas en cines"),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
       //El safe area ayuda a utilizar solo el área de la pantalla en la que no está el notch de algunos mobiles
-      body: SafeArea(
-        child: Text('Hola Mundo!!!!!!'),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //recurso del cual se sacará : https://pub.dev/packages/flutter_swiper
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      //con souble.infinity le decimos que use todo el ancho
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        //quitamos el control para que no se muestre las flechas
+        /* control: new SwiperControl(), */
       ),
     );
   }
